5fcec5032fbb519dcbe8521deba8b682a8b2a429
mtop <mats@topel.se>
2013-02-07 23:46:31 +0100

    Included a description of the program

--- geocoder.py
+++ geocoder.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+#	geocoder.py is a program written in Python that takes one file 
+#	containing polygons, and one file with species locality data 
+#	as input. The program then tests if a species have been recorded 
+#	inside any of the polygons. The result is presented as a nexux- 
+#	file with "0" indicating absence, and "1" indicating presence 
+#	in a polygon.
 #
-# Input:	See the example files.
+# 	Input:	See the example files localities.txt and polygons.txt. 
+# 	Output: 	See the example file ivesioids_out.nex.
+
+#	Copyright (C) 2013 Mats Töpel. 
 #
+#	Citation: If you use this version of the program, please cite;
+#	Mats Töpel (2013) Open Laboratory Notebook. www.matstopel.se
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from optparse import OptionParser
 
