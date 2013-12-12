554428101ab7926d2c70ab6598e21927360a4e52
Ruud Scharn <ruud.scharn@gmail.com>
2013-12-12 10:19:00 +0100

    Added GNU general public licence to script

--- utillities/geoinput.pl
+++ utillities/geoinput.pl
@@ -1,6 +1,21 @@
 #!/usr/bin/perl -w
 use strict;
 
+#	Copyright (C) 2013 Ruud Scharn. 
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#	
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ask for user input
 unless (@ARGV) {print "Please give an input 1 and input 2 and output file name"; exit;}
 
