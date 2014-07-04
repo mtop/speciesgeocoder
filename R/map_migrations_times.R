c62966301742059bd92e58f327155e803e233aae
dsilvestro <silvestro.daniele@gmail.com>
2014-07-04 09:12:35 +0200

    Added Ruud's relative migrations through time
    now it generates 2 plots with abs and del migrations

--- R/map_migrations_times.R
+++ R/map_migrations_times.R
@@ -35,6 +35,9 @@ option_list <- list(
 
     make_option("--d", default=F,type="logical",
         help="Verbose [default %default].")
+	
+	make_option("--b", default=1,type="integer",
+        help="Adjust binsize [default %default].")
 
     )
 
@@ -51,11 +54,14 @@ tbl_file=  opt$args[2]     #  "three_states.txt"   # "SGC_table.txt"  # "CAHighl
 tree_file= opt$args[3]     #  "1birdtree.nex"
 out_file=sprintf("%s.pdf", opt$options$o)
 out_table=sprintf("%s.txt", opt$options$o)
+out_file_rel=sprintf("%s_relative.txt", opt$options$o)
+out_table_rel=sprintf("%s_relative.txt", opt$options$o)
 n_rep=opt$options$r
 map_model=opt$options$m
 max_run_time=opt$options$s 
 verbose=opt$options$d
 no_char=opt$options$t
+agegap=$options$b 
 
 print(verbose)
 
@@ -75,10 +81,46 @@ setwd(wd)
 tbl= read.table(tbl_file,header=T,stringsAsFactors=F) # , sep="\t"
 area_name=colnames(tbl)
 tree <- read.nexus(tree_file)
-pdf(file=out_file,width=10, height=7)
+#pdf(file=out_file,width=10, height=7)
 
-####################################################################################
+#############################################################################################################
+
+branchtimes = branching.times(tree)
+maxmilcatogories = ceiling(max(branchtimes)/agegap)
+milgaps_mod=matrix(ncol=2, nrow=maxmilcatogories, 0)
+colnames(milgaps_mod)=c("time","tot_brl")
+milgaps_mod[,1]=1:maxmilcatogories*agegap
+
+v=branchtimes
+for(i in 1:length(milgaps_mod[,2])){
+        s=milgaps_mod[i,1]        # time start
+        e=milgaps_mod[i,1]-agegap # time end
+        l1= length(v[v>s])*agegap + min(length(v[v>s]),1)*agegap
+        l2= sum(v[v>e & v<s] -e)
+	if (l1==0){l2 = l2*2} # root
+        milgaps_mod[i,2]=l1+l2
+}
+
+#############################################################################################################
+
 F_calc <- function(res2){
+	M_ages=NULL 
+	for (i in 1:100){		
+		# draw rand uniform numbers
+		M_age=runif(n=length(res2[,1]),max=res2$age0,min=res2$age1)
+		h1=hist(M_age, breaks=bins,plot=F)
+		h1$counts = h1$counts/milgaps_mod[,2] #Make h1$counts relative to the total branchlenths
+		M_ages= rbind(M_ages,h1$counts)
+	}
+	x=as.data.frame(M_ages)   #q <- apply(x, 2, summary) #x=as.data.frame(M_ages)
+	m <- apply(x, 2, min)     #m = q[2,] #m <- apply(x, 2, min)
+	M <- apply(x, 2, max)     #M = q[5,] #M <- apply(x, 2, max)
+	a <- apply(x, 2, mean)    #a = q[3,] #a <- apply(x, 2, mean)
+	age=-h1$mids 
+	return(as.data.frame(cbind(m,M,a,age)))		
+}
+
+G_calc <- function(res2){
 	M_ages=NULL 
 	for (i in 1:100){		
 		# draw rand uniform numbers
@@ -86,18 +128,22 @@ F_calc <- function(res2){
 		h1=hist(M_age, breaks=bins,plot=F)
 		M_ages= rbind(M_ages,h1$counts)
 	}
-	x=as.data.frame(M_ages)   #q <- apply(x, 2, summary)
-	m <- apply(x, 2, min)     #m = q[2,]
-	M <- apply(x, 2, max)     #M = q[5,]
-	a <- apply(x, 2, mean)    #a = q[3,]
+	x=as.data.frame(M_ages)   #q <- apply(x, 2, summary) #x=as.data.frame(M_ages)
+	m <- apply(x, 2, min)     #m = q[2,] #m <- apply(x, 2, min)
+	M <- apply(x, 2, max)     #M = q[5,] #M <- apply(x, 2, max)
+	a <- apply(x, 2, mean)    #a = q[3,] #a <- apply(x, 2, mean)
 	age=-h1$mids 
 	return(as.data.frame(cbind(m,M,a,age)))		
 }
 
+F_plot <- function(L,title="Relative migrations through time",y_lim_M=10){
+	plot(L$age,L$a,type = 'n', ylim = c(0, y_lim_M), xlim = c(min(L$age),0), ylab = 'Relative migration events', xlab = 'Ma',main=title)
+	polygon(c(L$age, rev(L$age)), c(L$M, rev(L$m)), col = "#E5E4E2", border = NA)
+	lines(y=L$a, x=L$age, col = "#504A4B", border = NULL)
+}
 
-
-F_plot <- function(L,title="Migrations through time",y_lim_M=10){
-	plot(L$age,L$a,type = 'n', ylim = c(0, y_lim_M), xlim = c(min(L$age),0), ylab = 'migration events', xlab = 'Ma',main=title)
+G_plot <- function(L,title="Migrations through time",y_lim_MB=10){
+	plot(L$age,L$a,type = 'n', ylim = c(0, y_lim_MB), xlim = c(min(L$age),0), ylab = 'migration events', xlab = 'Ma',main=title)
 	polygon(c(L$age, rev(L$age)), c(L$M, rev(L$m)), col = "#E5E4E2", border = NA)
 	lines(y=L$a, x=L$age, col = "#504A4B", border = NULL)
 }
@@ -114,6 +160,8 @@ run_SM <- function(tree, trait,max_run){
 RES=list()
 effective_rep=0
 y_lim_M=0
+y_lim_MB=0
+
 for (replicate in 1:n_rep){
 	
 	cat("\nreplicate:", replicate,"\t")
@@ -136,7 +184,8 @@ for (replicate in 1:n_rep){
 			taxa[i]=tbl[i,1]
 		}
 	}
-	
+
+
 	rownames(trait)=taxa
 	if (no_char==0){
 		min_state=1
@@ -157,7 +206,8 @@ for (replicate in 1:n_rep){
 	if (length(trait)==0) {stop("\nNo matching taxa!\n")}
 	
 	branchtimes= branching.times(tree)
-	bins=0:ceiling(max(branchtimes))
+	bins=(0:maxmilcatogories)*agegap
+	
 
 	map=NULL
 	if (verbose==F){
@@ -197,11 +247,11 @@ for (replicate in 1:n_rep){
 		}
 		colnames(res)=c("edge", "age0", "age1","from","to")
 	
-		# calc mean, 95% CI migration events
+		# calc mean, 95% CI relative migration events
 		RES_temp=list()
 		RES_temp[[1]]=F_calc(res)
 		i=1
-		y_lim_M=max(y_lim_M,max(RES_temp[[1]]$M)) 
+		y_lim_M=max(y_lim_M,max(RES_temp[[1]]$M))
 		
 		directions=as.vector("global")
 		for (f in min_state:no_potential_states){
@@ -210,12 +260,11 @@ for (replicate in 1:n_rep){
 					i = i+1
 					#cat("\n",f,t,replicate)
 					res2=res[res$from==f & res$to==t,]
-					print(res2)
 					RES_temp[[i]]=F_calc(res2)
+					relmig=round(y_lim_M,digits = 4)
 					if (no_char==0){
-						directions[i]=sprintf("Migrations through time: %s -> %s (%s)",
-						area_name[f+1],area_name[t+1],length(res2[,1]))
-						}else{directions[i]=sprintf("Transition: %s -> %s (%s)",f,t,length(res2[,1]))} 
+						directions[i]=sprintf("Relative migrations through time: %s -> %s (%s)",area_name[f+1],area_name[t+1],length(res2[,1]))
+						}else{directions[i]=sprintf("Transition: %s -> %s (%s)",f,t,length(res2[,1]))}
 				}
 			}
 		}
@@ -224,31 +273,67 @@ for (replicate in 1:n_rep){
 			RES=RES_temp
 			}else{
 				for (i in 1:length(RES)){RES[[i]]=RES[[i]]+ RES_temp[[i]]}
-			}		
+			}	
+		# calc mean, 95% CI migration events
+		RES_temp=list()
+		RES_temp[[1]]=G_calc(res)
+		i=1
+		y_lim_MB=max(y_lim_MB,max(RES_temp[[1]]$M))
+		
+		directionsB=as.vector("global")
+		for (f in min_state:no_potential_states){
+			for (t in min_state:no_potential_states) {
+				if  (f != t) {
+					i = i+1
+					#cat("\n",f,t,replicate)
+					res2=res[res$from==f & res$to==t,]
+					RES_temp[[i]]=G_calc(res2)
+					mig=y_lim_MB
+					if (no_char==0){
+						directionsB[i]=sprintf("Migrations through time: %s -> %s (%s)",area_name[f+1],area_name[t+1],length(res2[,1]))
+						}else{directionsB[i]=sprintf("Transition: %s -> %s (%s)",f,t,length(res2[,1]))}
+				}
+			}
+		}
+		## average results over SMs
+		if (replicate==1){
+			RESB=RES_temp
+			}else{
+				for (i in 1:length(RESB)){RESB[[i]]=RESB[[i]]+ RES_temp[[i]]}
+			}
 	}else{cat("Time limit reached!")}
 }
 
 cat(sprintf("# Headers: min, max, and average (m, M, a) number of migration events through time (age) averaged over %s stochastic maps.\n", effective_rep),file=out_table)
+cat(sprintf("# Headers: min, max, and average (m, M, a) relative number of migration events through time (age) averaged over %s stochastic maps.\n", effective_rep),file=out_table_rel)
 
 # make plots/output table
+pdf(file=out_file_rel,width=10, height=7)
 for (i in 1:length(RES)){
 	counts=RES[[i]]/effective_rep
 	F_plot(counts,directions[i],y_lim_M)
-	cat(sprintf("# Table %s (%s).\n",i,directions[i]), file=out_table,append = T)
+	cat(sprintf("# Table %s (%s).\n",i,directions[i]), file=out_table_rel,append = T)
 	row.names(counts)=paste0(row.names(counts), sep="_",rep(directions[i], length(counts[,1])))
+	if (i==1){
+		suppressWarnings(write.table(counts, file=out_table_rel,append = T,sep="\t",row.names=T,col.names=T))
+		}else{suppressWarnings(write.table(counts, file=out_table_rel,append = T,sep="\t",row.names=T,col.names=F))}
+	
+}
+suppressMessages(dev.off())
+
+pdf(file=out_file,width=10, height=7)
+for (i in 1:length(RESB)){
+	counts=RESB[[i]]/effective_rep
+	G_plot(counts,directionsB[i],y_lim_MB)
+	cat(sprintf("# Table %s (%s).\n",i,directionsB[i]), file=out_table,append = T)
+	row.names(counts)=paste0(row.names(counts), sep="_",rep(directionsB[i], length(counts[,1])))
 	if (i==1){
 		suppressWarnings(write.table(counts, file=out_table,append = T,sep="\t",row.names=T,col.names=T))
 		}else{suppressWarnings(write.table(counts, file=out_table,append = T,sep="\t",row.names=T,col.names=F))}
 	
 }
 
-suppressMessages((dev.off())
-
-
-
-
-
-
+suppressMessages(dev.off())
 
 
 
