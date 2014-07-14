daa7703356780697290c47c05b60eb7f1e410473
Mats Töpel <mats@topel.se>
2014-07-15 00:15:54 +0200

    Moved preparation of files the plooting function to separate module

--- geocoder.py
+++ geocoder.py
@@ -26,7 +26,7 @@ try:
 	import argparse
 except ImportError:
 	sys.stderr.write("[Error] The python module \"argparse\" is not installed\n")
-	sys.stderr.write("[--] Would you like to install it now [Y/N]? ")
+	sys.stderr.write("[--] Would you like to install it now using 'sudo easy_install' [Y/N]? ")
 	answer = sys.stdin.readline()
 	if answer[0].lower() == "y":
 		sys.stderr.write("[--] Running \"sudo easy_install argparse\"\n")
@@ -445,57 +445,9 @@ def main():
 
 
 	if args.plot == True:
-	### Go to R ###
-	### Do tests of cutoff values and call R + functions
-
-		### Prepare the input files for R
-		
-		# coordinates.sgc.txt
-		out1 = open("coordinates.sgc.txt", "w")
-		out1.write("identifier\tXCOOR\tYCOOR\n")
-		for species, polygon, longitude, latitude in result.getSampletable():
-			# Note that the latitude/longitude order has shifted 
-			# in order to fit the requirements of the R code.
-			out1.write("%s\t%s\t%s\n" % (species.replace(" ", "_") , longitude, latitude))
-		out1.close()
-
-		# polygons.sgc.txt
-		out2 = open("polygons.sgc.txt", "w")
-		out2.write("identifier\tXCOOR\tYCOOR\n")
-		for polygon in polygons.getPolygons():
-			for coordPair in polygon[1]:
-				# Note that the latitude/longitude order has shifted
-				# in order to fit the requirements of the R code.
-				out2.write("%s\t%s\t%s\n" % (polygon[0].replace(" ", "_"), coordPair.split(' ')[0], coordPair.split(' ')[1]))
-		out2.close()
-
-		# sampletable.sgc.txt
-		out3 = open("sampletable.sgc.txt", "w")
-		out3.write("identifier\thomepolygon\tXCOOR\tYCOOR\n")
-		for species, polygon, longitude, latitude in result.getSampletable():
-			out3.write("%s\t%s\t%s\t%s\n" % (species.replace(" ", "_"), polygon.replace(" ", "_"), longitude, latitude))
-		out3.close()
-
-		# speciestable.sgc.txt
-		# Number of occurrences per polygon
-		out4 = open("speciestable.sgc.txt", "w")
-		# Headers 
-		header = "Species\t"
-		for name in result.getPolygonNames():
-			header += "%s\t" % name.replace(" ", "_")
-		header = header[:-1] + "\n"
-		out4.write(header)
-		# Species names and character matrix
-		for species in result.getResult():
-			string = "%s\t" % species.replace(" ", "_")
-			for record in result.getResult()[species]:
-				string += "%s\t" % record 
-			string = string[:-1] + "\n"
-			out4.write(string)
-		out4.close()
-
-		### Test of the plot function
 		import os
+		from lib.plot import prepare_plots
+		prepare_plots(result, polygons)
 		wd = os.getcwd()                    # Working directory
 		cmd="Rscript R/graphical_output.R %s %s %s %s %s" \
 		% (wd, "coordinates.sgc.txt", "polygons.sgc.txt", "sampletable.sgc.txt", "speciestable.sgc.txt")
