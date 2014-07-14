801a2fde75b211c4ad4944b76e02f07e5c8ff84e
Mats Töpel <mats@topel.se>
2014-07-14 23:43:44 +0200

    By default using osgeo rather then compiled version of the GDAL programs. This seem to influence the speed of the code significantly.

--- lib/readGeoTiff.py
+++ lib/readGeoTiff.py
@@ -20,52 +20,61 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+import os
 import subprocess
+
+try:
+	from osgeo import gdal
+except ImportError:
+	sys.stderr.write("[Error] The python module \"osgeo\" is not installed\n")
+	sys.stderr.write("[--] Would you like to install it now using 'sudo easy_install GDAL' [Y/N]? ")
+	answer = sys.stdin.readline()
+	if answer[0].lower() == "y":
+		# Try to install GDAL. This may very well fail...
+		try:
+			sys.stderr.write("[--] Running \'sudo easy_install GDAL\'\n")
+			subprocess.check_call(["sudo", "easy_install", "GDAL"])
+		except:
+			sys.stderr.write("[Error] \'sudo easy_install GDAL\' failed.\n")
+			sys.exit("[Error] Please visit https://pypi.python.org/pypi/GDAL/ for further installation instructions.")
+
 
 class Geotiff(object):
 	# GeoTiff object
 	def __init__(self, tiffile):
 		self.tiffile = tiffile
-		try:
-#			print "[--] Trying to use python GDAL"		# Devel.
-			from osgeo import gdal
-#			import zzz									# Devel.
-#			print "[--] Using python GDAL"				# Devel.
-			my_file = gdal.Open(self.tiffile)
-			self.width = my_file.RasterXSize
-			self.height = my_file.RasterYSize
-			self.gt = my_file.GetGeoTransform()
-			self.MINX = self.gt[0]
-			self.MINY = self.gt[3] + self.width*self.gt[4] + self.height*self.gt[5]
-			self.MAXX = self.gt[0] + self.width*self.gt[1] + self.height*self.gt[2]
-			self.MAXY = self.gt[3]
-		except:
-			from os.path import abspath, dirname, join
-#			print "[--] Using C GDAL instead"			# Devel.
-			if sys.platform.startswith('linux2'):
-				binary = abspath(join(dirname(__file__), "../bin/gdalinfo_linux"))
-#				sys.exit("GDAL not installed")				# For now
-			if sys.platform.startswith('darwin'):
-				binary = abspath(join(dirname(__file__), "../bin/gdalinfo_darwin"))
-			if sys.platform.startswith('cygwin'):
-				binary = abspath(join(dirname(__file__), "../bin/gdalinfo_cygwin"))
-			self.out = subprocess.check_output([binary, self.tiffile])
+		# Use the python version of GDAL by default.
+		self.tiffile = gdal.Open(self.tiffile)
+		self.width = self.tiffile.RasterXSize
+		self.height = self.tiffile.RasterYSize
+		self.gt = self.tiffile.GetGeoTransform()
+		self.MINX = self.gt[0]
+		self.MINY = self.gt[3] + self.width*self.gt[4] + self.height*self.gt[5]
+		self.MAXX = self.gt[0] + self.width*self.gt[1] + self.height*self.gt[2]
+		self.MAXY = self.gt[3]
 
-			upper_left = self.out.split("Upper Left  (")[1].split(")")[0].split("\n")
-			lower_left = self.out.split("Lower Left  (")[1].split(")")[0].split("\n")
-			upper_right = self.out.split("Upper Right (")[1].split(")")[0].split("\n")
-			lower_right = self.out.split("Lower Right (")[1].split(")")[0].split("\n")
-
-			self.MINY = lower_left[0].split(",")[1]
-			self.MINX = lower_left[0].split(",")[0]		# OK
-			self.MAXY = upper_right[0].split(",")[1]
-			self.MAXX = lower_right[0].split(",")[0]	# OK
-
-#            minx: 17.9998611111
-#            maxx: 19.0001388889
-#            miny: 56.9998611111
-#            maxy: 58.0001388889
-
+		# Legacy code.
+		# Used if compiled versions of the GDAL library should be used instead of "osgeo".
+#		else:
+#			from os.path import abspath, dirname, join
+#			# If this is not a Windows system ...
+#			if not sys.platform.startswith('win32'):
+#				self.out = subprocess.check_output(["gdalinfo", self.tiffile])
+#
+#				upper_left = self.out.split("Upper Left  (")[1].split(")")[0].split("\n")
+#				lower_left = self.out.split("Lower Left  (")[1].split(")")[0].split("\n")
+#				upper_right = self.out.split("Upper Right (")[1].split(")")[0].split("\n")
+#				lower_right = self.out.split("Lower Right (")[1].split(")")[0].split("\n")
+#	
+#				self.MINY = lower_left[0].split(",")[1]
+#				self.MINX = lower_left[0].split(",")[0]		# OK
+#				self.MAXY = upper_right[0].split(",")[1]
+#				self.MAXX = lower_right[0].split(",")[0]	# OK
+#
+#			else:
+#				# This is a Windows system
+#				# Add some code...
+#				pass
 
 	def minx(self):
 		return float(self.MINX)
@@ -83,24 +92,38 @@ class Geotiff(object):
 		return "MinX: ", self.minx(), "MaxX: ", self.maxx(), "MinY: ", self.miny(), "MaxY: ", self.maxy()
 
 	def get_elevation(self, lon, lat):
-		try:
-			# Use preinstalled verwion of "gdallocationinfo" if exists...
-			elevation = subprocess.check_output(["gdallocationinfo", "-valonly", "-wgs84", self.tiffile, lon, lat])
-		except:
-			try:
-				# ...else, try the precompiled version of "gdallocationinfo".
-				from os.path import abspath, dirname, join
-				if sys.platform.startswith('linux2'):
-					binary = abspath(join(dirname(__file__), "../bin/gdallocationinfo_linux2"))
-				if sys.platform.startswith('darwin'):
-					binary = abspath(join(dirname(__file__), "../bin/gdallocationinfo_darwin"))
-				if sys.platform.startswith('cygwin'):
-					binary = abspath(join(dirname(__file__), "../bin/gdallocationinfo_cygwin"))
-				elevation = subprocess.check_output([binary, "-valonly", "-wgs84", self.tiffile, lon, lat])
-			except:
-				# If that does not work, promt the user to compile GDAL.
-				sys.exit("\n\n[ERROR] Could not find the program \"gdallocationinfo\" that \n\tis needed for SpeciesGeoCoder to extract elevation \n\tdata from geotif files. Please read the INSTALL file \n\tfor instructions on how to install GDAL.\n")
+		import struct
+		# Thanks 'Luke'. http://gis.stackexchange.com/questions/46893/
+		# how-do-i-get-the-pixel-value-of-a-gdal-raster-under-an-ogr-point-without-numpy
+		px = int((float(lon) - self.gt[0]) / self.gt[1]) # x pixel
+		py = int((float(lat) - self.gt[3]) / self.gt[5]) # y pixel
+		raster_band = self.tiffile.GetRasterBand(1)	
+		structval = raster_band.ReadRaster(px, py, 1, 1, buf_type=gdal.GDT_UInt16) # Assumes 16 bit int aka 'short'
+		out = struct.unpack('h' , structval) # Use the 'short' format code (2 bytes) not int (4 bytes)
+		elevation = out[0]
+		# Legacy code.
+#		else:
+#			# Use preinstalled version of "gdallocationinfo" if exists...
+#			elevation = subprocess.check_output(["gdallocationinfo", "-valonly", "-wgs84", self.tiffile, lon, lat])
 		return int(elevation)	
+
+		
+		
+# Determine if binary versions of the GDAL programs "gdallocationinfo" 
+# and "gdalinfo" are found in $PATH.
+def gdal_installed():
+	installed = []
+	program_list = ["gdallocationinfo", "gdalinfo"]
+	for program in program_list:
+		for path in os.environ["PATH"].split(os.pathsep):
+			full_path = os.path.join(path, program)
+			if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+				installed.append(program)
+	if "gdallocationinfo" and "gdalinfo" in installed:
+		return True
+	else:
+		return False
+
 
 	
 def indexTiffs(infiles):
